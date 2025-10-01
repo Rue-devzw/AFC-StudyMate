de88226a79f421d08609243fcfb9b12982c14109
rufarobernards <rufarobernards@gmail.com>
2025-10-01 14:43:47 +0000

    fix

--- lib/screens/bible_screen.dart
+++ lib/screens/bible_screen.dart
@@ -1,11 +1,12 @@
-import '../bible_provider.dart';
-import '/models/bible.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+
 import 'book_screen.dart';
 import 'search_screen.dart';
-import '/services/bible_service.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-import 'package:provider/provider.dart';
+import '../bible_provider.dart';
+import '../models/bible.dart';
+import '../services/bible_service.dart';
 
 class BibleScreen extends StatefulWidget {
   const BibleScreen({super.key});
@@ -27,6 +28,7 @@ class _BibleScreenState extends State<BibleScreen> {
   @override
   Widget build(BuildContext context) {
     final bibleProvider = Provider.of<BibleProvider>(context);
+    final bibleId = bibleProvider.selectedBibleId;
 
     return Scaffold(
       appBar: AppBar(
@@ -55,67 +57,73 @@ class _BibleScreenState extends State<BibleScreen> {
           ),
         ],
       ),
-      body: FutureBuilder<List<Book>>(
-        future: BibleService.getBooks(bibleProvider.selectedBibleId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No books found.'));
-          } else {
-            final books = snapshot.data!;
-            return AnimationLimiter(
-              child: GridView.builder(
-                padding: const EdgeInsets.all(8.0),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 2.5,
-                  crossAxisSpacing: 8.0,
-                  mainAxisSpacing: 8.0,
-                ),
-                itemCount: books.length,
-                itemBuilder: (context, index) {
-                  final book = books[index];
-                  return AnimationConfiguration.staggeredGrid(
-                    position: index,
-                    duration: const Duration(milliseconds: 375),
-                    columnCount: 2,
-                    child: ScaleAnimation(
-                      child: FadeInAnimation(
-                        child: Card(
-                          elevation: 2.0,
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => BookScreen(book: book),
-                                ),
-                              );
-                            },
-                            child: Center(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  book.name,
-                                  textAlign: TextAlign.center,
-                                  style: Theme.of(context).textTheme.titleMedium,
+      body: bibleId == null
+          ? const Center(child: Text('No Bible selected.'))
+          : FutureBuilder<List<Book>>(
+              future: BibleService.getBooks(bibleId: bibleId),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No books found.'));
+                } else {
+                  final books = snapshot.data!;
+                  return AnimationLimiter(
+                    child: GridView.builder(
+                      padding: const EdgeInsets.all(8.0),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 2.5,
+                        crossAxisSpacing: 8.0,
+                        mainAxisSpacing: 8.0,
+                      ),
+                      itemCount: books.length,
+                      itemBuilder: (context, index) {
+                        final book = books[index];
+                        return AnimationConfiguration.staggeredGrid(
+                          position: index,
+                          duration: const Duration(milliseconds: 375),
+                          columnCount: 2,
+                          child: ScaleAnimation(
+                            child: FadeInAnimation(
+                              child: Card(
+                                elevation: 2.0,
+                                child: InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            BookScreen(book: book),
+                                      ),
+                                    );
+                                  },
+                                  child: Center(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        book.name,
+                                        textAlign: TextAlign.center,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleMedium,
+                                      ),
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
+                        );
+                      },
                     ),
                   );
-                },
-              ),
-            );
-          }
-        },
-      ),
+                }
+              },
+            ),
     );
   }
 }
